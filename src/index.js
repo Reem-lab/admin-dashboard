5e8bcd6d3010b0581db46a3b4f2f1c8136e65920
reem <janinareem@gmail.com>
2022-11-20 02:10:38 +0200

    Add basic set up

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BroswerRouter } from 'react-router-dom';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <BroswerRouter>
+     <App />
+  </BroswerRouter>
+  </React.StrictMode>
+);
+
+
